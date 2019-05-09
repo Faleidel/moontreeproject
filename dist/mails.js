6f0aaa9915dee8fba39ee4c4e10212a29f460909
Your Name <you@example.com>
2019-05-09 20:06:08 +0000

    Started email service integration.

---
+++ dist/mails.js
@@ -0,0 +1,36 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const utils = __importStar(require("./utils"));
+const sendGrid = require("@sendgrid/mail");
+if (utils.config.sendGridKey)
+    sendGrid.setApiKey(utils.config.sendGridKey);
+else
+    console.log("No sendgrid api key in config (config.sendGridKey)");
+;
+function sendMail(mail) {
+    sendGrid.send(Object.assign({}, mail, { from: "admin@" + utils.host }));
+}
+exports.sendMail = sendMail;
+function sendAdmin(mail) {
+    if (!utils.config.adminEmail) {
+        utils.log("Error with sendAmin, no email in config");
+    }
+    else {
+        sendMail(Object.assign({}, mail, { to: utils.config.adminEmail }));
+    }
+}
+exports.sendAdmin = sendAdmin;
+function sendAdminAlert(content) {
+    sendAdmin({
+        subject: "admin alert",
+        text: content
+    });
+}
+exports.sendAdminAlert = sendAdminAlert;
---
+++ src/mails.ts
@@ -0,0 +1,43 @@
+import * as utils from "./utils";
+const sendGrid = require("@sendgrid/mail");
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+if (utils.config.sendGridKey)
+    sendGrid.setApiKey(utils.config.sendGridKey);
+else
+    console.log("No sendgrid api key in config (config.sendGridKey)");
+
+interface Mail {
+    to: string | string[],
+    subject: string,
+    text?: string,
+    html?: string
+};
+
+type AnonymousMail = Omit<Mail, 'to'>;
+
+export function sendMail(mail: Mail): void {
+    sendGrid.send({
+        ...mail,
+        from: "admin@" + utils.host
+    });
+}
+
+export function sendAdmin(mail: AnonymousMail): void {
+    if (!utils.config.adminEmail) {
+        utils.log("Error with sendAmin, no email in config");
+    } else {
+        sendMail({
+            ...mail,
+            to: utils.config.adminEmail
+        });
+    }
+}
+
+export function sendAdminAlert(content: string) {
+    sendAdmin({
+        subject: "admin alert",
+        text: content
+    });
+}
